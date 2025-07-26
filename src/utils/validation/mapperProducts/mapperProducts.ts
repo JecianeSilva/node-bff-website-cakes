6bacc62d3ac6e801cf36c0ba6cada3800c65f7cc
Jeciane Silva <jeciane_amorim@hotmail.com>
2025-07-26 11:01:52 -0300

    fix: rename imageUrl

--- src/utils/validation/mapperProducts/mapperProducts.ts
+++ src/utils/validation/mapperProducts/mapperProducts.ts
@@ -1,12 +1,12 @@
 import { IProduct, ISimplifiedProduct } from "cakes-lib-types-js";
 
 export function mapperProducts(products: IProduct[]): ISimplifiedProduct[] {
-  return products.map(({ id, name, description, price, image_url, category }) => ({
+  return products.map(({ id, name, description, price, imageUrl, category }) => ({
     id,
     name,
     description,
     price,
-    image_url,
+    imageUrl,
     category: category.name
   })) as ISimplifiedProduct[];
 }
